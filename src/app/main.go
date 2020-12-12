3f6da1ddb37531e4062b7179edce637908e055e0
Jonathan Tobe <jdtobe@gmail.com>
2020-12-12 03:42:28 -0800

    Separated out routines for drawing LEDs in memory to the light strip, drawing LEDs to memory, and fading out LEDs."

--- src/app/main.go
+++ src/app/main.go
@@ -7,6 +7,7 @@ import (
 	"runtime/debug"
 	"time"
 
+	colorful "github.com/lucasb-eyer/go-colorful"
 	ws2811 "github.com/rpi-ws281x/rpi-ws281x-go"
 )
 
@@ -36,16 +37,21 @@ func runServer() {
 }
 
 const (
-	// ledLum = 128
-	ledLum = 200
-	ledNum = 50
-
-	// colorRed = uint32(0x00FF00)
-	colorRed   = uint32(0xFF0000)
-	colorBlue  = uint32(0x00FF00)
-	colorGreen = uint32(0x0000FF)
-	colorWhite = uint32(0xFFFFFF)
+	ledLum = 128
+	// ledLum = 200
+	ledNum = 100
 )
+
+var (
+	colorRed   = hsv{H: 0, S: 1.0, V: 1.0}
+	colorBlue  = hsv{H: 235, S: 1.0, V: 1.0}
+	colorGreen = hsv{H: 135, S: 1.0, V: 1.0}
+	colorWhite = hsv{H: 0, S: 0.0, V: 1.0}
+)
+
+type hsv struct {
+	H, S, V float64
+}
 
 func doLights() {
 	opt := ws2811.DefaultOptions
@@ -59,16 +65,102 @@ func doLights() {
 	checkError(m.Init())
 	defer m.Fini()
 
-	leds := []uint32{colorRed, colorWhite, colorGreen, colorWhite}
-	l := len(leds)
-	offset := 0
-	for {
-		for i := 0; i < ledNum; i++ {
-			m.Leds(0)[i] = leds[(i+offset)%l]
-		}
+	ledChain := []hsv{colorRed, colorWhite, colorGreen, colorWhite}
 
-		checkError(m.Render())
-		offset = (offset + 1) % l
-		time.Sleep(250 * time.Millisecond)
+	// muLeds := sync.RWMutex{}
+	leds := [ledNum]hsv{}
+
+	done := make(chan struct{})
+
+	// Setup Faders
+	go func() {
+		for {
+			select {
+			case <-done:
+				log.Printf("Exiting: Fader")
+				return
+			default:
+			}
+
+			// muLeds.Lock()
+			for i := range leds {
+				leds[i].V = leds[i].V * 0.98
+			}
+			// muLeds.Unlock()
+
+			time.Sleep(5 * time.Millisecond)
+			// angle := float64(0.0)
+			// speed := 1.0 - math.Sin(angle)
+			// time.Sleep(time.Duration(int64(speed * 10 * float64(time.Millisecond))))
+			// angle = angle + math.Pi/50
+			// if twopi := (2 * math.Pi); angle > twopi {
+			// 	angle = angle - twopi
+			// }
+		}
+	}()
+
+	// Run Draw Loop
+	go func() {
+		offset := 0
+		ledChainLen := len(ledChain)
+		for {
+			select {
+			case <-done:
+				log.Println("Exiting; Draw Loop")
+				return
+			default:
+			}
+
+			// Set LED Colors:
+			const split = 10
+			for j := 0; j < ledNum/split; j++ {
+				// muLeds.Lock()
+				for k := j; k < ledNum; k += ledNum / split {
+					leds[k] = ledChain[(k+offset)%len(ledChain)]
+				}
+				// muLeds.Unlock()
+				time.Sleep(100 * time.Millisecond)
+			}
+			offset = (offset + 1) % ledChainLen
+		}
+	}()
+
+	// Show
+	go func() {
+		ticker := time.NewTicker(time.Millisecond)
+
+		for {
+			select {
+			case <-done:
+				log.Println("Exiting: Show")
+				return
+			case <-ticker.C:
+				// muLeds.RLock()
+				for i := 0; i < ledNum; i++ {
+					r, g, b := colorful.Hsv(leds[i].H, leds[i].S, leds[i].V).RGB255()
+					m.Leds(0)[i] = (uint32(r)<<8+uint32(b))<<8 + uint32(g)
+				}
+				// muLeds.RUnlock()
+				checkError(m.Render())
+			}
+		}
+	}()
+
+	select {
+	case <-done:
+		log.Println("System Exiting!")
 	}
+
+	// leds := []uint32{colorRed, colorWhite, colorGreen, colorWhite}
+	// l := len(leds)
+	// offset := 0
+	// for {
+	// 	for i := 0; i < ledNum; i++ {
+	// 		m.Leds(0)[i] = leds[(i+offset)%l]
+	// 	}
+
+	// 	checkError(m.Render())
+	// 	offset = (offset + 1) % l
+	// 	time.Sleep(250 * time.Millisecond)
+	// }
 }
--- src/go.mod
+++ src/go.mod
@@ -3,6 +3,7 @@ module github.com/jdtobe/Zero
 go 1.15
 
 require (
+	github.com/lucasb-eyer/go-colorful v1.0.3
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rpi-ws281x/rpi-ws281x-go v1.0.6
 )
